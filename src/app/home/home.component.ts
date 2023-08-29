7b486b6517f82f0ab1b9c47e3a9a205a07cc4fc4
Danward Bautista <danwarddeveloper@gmail.com>
2023-08-30 01:00:33 +0800

    Home Component

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { faLock, faLockOpen, faSatellite, faEnvelope, faEnvelopeOpenText, faWifi, faEye, faSatelliteDish, faCheckCircle, } from '@fortawesome/free-solid-svg-icons'
 
 @Component({
   selector: 'app-home',
@@ -6,5 +7,15 @@ import { Component } from '@angular/core';
   styleUrls: ['./home.component.scss']
 })
 export class HomeComponent {
-
+  faSatellite = faSatellite;
+  faEnvelope = faEnvelope;
+  faEnvelopeOpenText = faEnvelopeOpenText;
+  faWifi = faWifi;
+  // faWifiSlash = faWifiSlash;
+  faEye = faEye;
+  faSatelliteDish = faSatelliteDish;
+  faCheckCircle = faCheckCircle;
+  // faMailbox = faMailbox;
+  faLock = faLock;
+  faLockOpen = faLockOpen;
 }
