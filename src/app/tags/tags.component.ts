1996c481b214ab4daf6c6c75f0820020c1e1272a
Danward Bautista <danwarddeveloper@gmail.com>
2023-08-30 02:53:31 +0800

    Tags Component

--- src/app/tags/tags.component.ts
+++ src/app/tags/tags.component.ts
@@ -1,4 +1,8 @@
-import { Component } from '@angular/core';
+import { AfterViewInit, Component, ViewChild } from '@angular/core';
+import { MatPaginator } from '@angular/material/paginator';
+import { MatTableDataSource } from '@angular/material/table';
+import { ModalDismissReasons, NgbDatepickerModule, NgbModal } from '@ng-bootstrap/ng-bootstrap';
+import {MatSort, Sort} from '@angular/material/sort'
 
 @Component({
   selector: 'app-tags',
@@ -6,5 +10,51 @@ import { Component } from '@angular/core';
   styleUrls: ['./tags.component.scss']
 })
 export class TagsComponent {
+  displayedColumns: string[] = ['name', 'action'];
+  dataSource = new MatTableDataSource<PeriodicElement>(ELEMENT_DATA);
 
+  constructor(private modalService: NgbModal) { }
+
+  @ViewChild(MatPaginator) paginator!: MatPaginator;
+  @ViewChild(MatSort) sort!: MatSort;
+
+  ngAfterViewInit() {
+    this.dataSource.paginator = this.paginator;
+    this.dataSource.sort = this.sort;
+  }
+
+  applyFilter(event: Event) {
+    const filterValue = (event.target as HTMLInputElement).value;
+    this.dataSource.filter = filterValue.trim().toLowerCase();
+  }
+
+  open(content: any) {
+    this.modalService.open(content);
+
+    console.log('click');
+  }
+
+  openSm(content: any) {
+    this.modalService.open(content, { size: 'sm' });
+  }
+
+  openLg(content: any) {
+    this.modalService.open(content, { size: 'lg' });
+  }
+
+  openXl(content: any) {
+    this.modalService.open(content, { size: 'xl' });
+  }
 }
+
+export interface PeriodicElement {
+  name: string;
+  color:string;
+  description: string;
+}
+
+const ELEMENT_DATA: PeriodicElement[] = [
+    { name: "Moderator", color:"#eb4034", description: "This is an example tag"},
+    { name: "Admin", color:"#403875", description: "This is an example tag"},
+    { name: "Employee", color:"#429120", description: "This is an example tag"},
+];
